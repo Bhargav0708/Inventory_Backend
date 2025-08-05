9d28e00c03b526b7944ce20ada06bc911b271aa7
bhargav.harvani <bhargav.harvani@esparkbizmail.com>
2025-08-05 19:22:42 +0530

    production added

--- src/config/config.js
+++ src/config/config.js
@@ -29,11 +29,12 @@ module.exports = {
     host: "127.0.0.1",
     dialect: "mysql",
   },
+
   production: {
-    username: "root",
-    password: null,
-    database: "database_production",
-    host: "127.0.0.1",
-    dialect: "mysql",
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST2,
+    dialect: process.env.DB_DIALECT,
   },
 };
