4da76c7fcc155417ee3d915c14c236d3f3e60dfa
bhargav.harvani <bhargav.harvani@esparkbizmail.com>
2025-08-07 13:19:45 +0530

    Url Changed or commited

--- src/index.ts
+++ src/index.ts
@@ -21,7 +21,6 @@ import { callbackPromise } from "nodemailer/lib/shared";
 
 const app = express();
 
-// const allowedOrigins = [process.env.FRONTEND_URL, " http://localhost:5173"];
 app.use(
   cors({
     origin: process.env.FRONTEND_URL,
