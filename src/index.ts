64ddbfdbd500e8894b546b2f2f7662e054d2c110
bhargav.harvani <bhargav.harvani@esparkbizmail.com>
2025-08-07 14:12:24 +0530

    Url changed

--- src/index.ts
+++ src/index.ts
@@ -20,7 +20,8 @@ import cart from "./routes/cart.routes";
 import { callbackPromise } from "nodemailer/lib/shared";
 
 const app = express();
-//
+//ggd
+//ggd
 app.use(
   cors({
     origin: process.env.FRONTEND_URL,
